e138d31e8bba0f0e35cb59cb42e40aba8222bc2d
albcasmor3 <achantacalabuth@gmail.com>
2025-10-17 13:41:17 +0200

    metodos de filtrado

--- src/GestorPeliculas.java
+++ src/GestorPeliculas.java
@@ -1,6 +1,39 @@
+import java.util.ArrayList;
 import java.util.List;
 
-public class GestorPeliculas {
-    List<Pelicula> peliculas;
+public  class GestorPeliculas {
+    static List<pelicula> peliculas = new ArrayList<>();
 
+    public record pelicula(String titulo, String director, String genero, int duracion){
+
+    }
+
+    static void filtrarPeliculasGenero(String g){
+        for (pelicula pelito : peliculas) {
+
+            if (pelito.genero.toLowerCase().contains(g.toLowerCase())) {
+
+                System.out.println(pelito);
+            }
+        }
+    }
+    static void filtrarPeliculasDirector(String d){
+        for (pelicula pelito : peliculas) {
+
+            if (pelito.director.toLowerCase().contains(d.toLowerCase())) {
+
+                System.out.println(pelito);
+            }
+        }
+    }
+
+
+
+    static void main() {
+        pelicula peli = new pelicula("Pasta","Jaume","Horror",120);
+        peliculas.add(peli);
+        filtrarPeliculasGenero("Horror");
+        filtrarPeliculasDirector("Jaume");
+    }
 }
+
