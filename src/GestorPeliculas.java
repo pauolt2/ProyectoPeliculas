e66835c9db5155cc4d529f4d305f4efa61d6f364
Sxz3 <redjaxy28@gmail.com>
2025-10-23 14:25:54 +0200

    Arreglos.

--- src/GestorPeliculas.java
+++ src/GestorPeliculas.java
@@ -4,29 +4,25 @@ import java.util.List;
 public class GestorPeliculas {
     static List<Pelicula> catalogo = new ArrayList<>();
     private static final String ARCHIVO = "datos/peliculas.csv";
-  
-  
+
     public static boolean añadirPelicula(Pelicula pelicula){
         return catalogo.add(pelicula);
-    
-    
-    static void filtrarPeliculasGenero(String g){
-        for (Pelicula pelito : catalogo) {
-            if (pelito.genero.toLowerCase().contains(g.toLowerCase())) {
+    }
 
+    public static void filtrarPeliculasGenero(String g){
+        for (Pelicula pelito : catalogo) {
+            if (pelito.getGenero().toLowerCase().contains(g.toLowerCase())) {
                 System.out.println(pelito);
             }
         }
     }
-      
-    static void filtrarPeliculasDirector(String d){
+
+    public static void filtrarPeliculasDirector(String d){
         for (Pelicula pelito : catalogo) {
-
-            if (pelito.director.toLowerCase().contains(d.toLowerCase())) {
-
+            if (pelito.getDirector().toLowerCase().contains(d.toLowerCase())) {
                 System.out.println(pelito);
             }
         }
     }
+
 }
-
