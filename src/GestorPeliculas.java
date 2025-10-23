53a12f0c38933e49686329464664ceb4860468e7
Sxz3 <redjaxy28@gmail.com>
2025-10-23 14:17:23 +0200

    Añadir pelicula

--- src/GestorPeliculas.java
+++ src/GestorPeliculas.java
@@ -2,12 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GestorPeliculas {
-    private static List<Pelicula> catalogo = new ArrayList<>();
+    static List<Pelicula> catalogo = new ArrayList<>();
     private static final String ARCHIVO = "datos/peliculas.csv";
 
     public static boolean añadirPelicula(Pelicula pelicula){
         return catalogo.add(pelicula);
     }
-
-
 }
