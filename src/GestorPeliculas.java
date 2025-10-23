d920cf39b2b09c9891688bc1c501089ba8ce2428
Sxz3 <redjaxy28@gmail.com>
2025-10-23 14:27:32 +0200

    Implementado verCatalogo

--- src/GestorPeliculas.java
+++ src/GestorPeliculas.java
@@ -5,8 +5,21 @@ public class GestorPeliculas {
     private static List<Pelicula> catalogo = new ArrayList<>();
     private static final String ARCHIVO = "datos/peliculas.csv";
 
-    public static boolean añadirPelicula(Pelicula pelicula){
-        return catalogo.add(pelicula);
+    public static void verCatalogo() {
+        if (catalogo.isEmpty()) {
+            System.out.println("El catálogo está vacío.");
+        } else {
+            System.out.println("Catálogo de películas:");
+            for (Pelicula p : catalogo) {
+                System.out.println("----------------------------------");
+                System.out.println("Título: " + p.getTitulo());
+                System.out.println("Director: " + p.getDirector());
+                System.out.println("Año de estreno: " + p.getEstreno());
+                System.out.println("Género: " + p.getGenero());
+                System.out.println("Valoración: " + p.getValoracion() + "/5");
+            }
+            System.out.println("----------------------------------");
+        }
     }
 
 
