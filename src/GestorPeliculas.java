44f74f86e5fa2dc0fce6189316ae2e6b0c2b6f1d
ferra <ferranlatorre6@gmail.com>
2025-10-29 13:20:07 +0100

    nuevo

--- src/GestorPeliculas.java
+++ src/GestorPeliculas.java
@@ -7,6 +7,49 @@ public class GestorPeliculas {
     static List<Pelicula> catalogo = new ArrayList<>();
     private static final String ARCHIVO = "datos/peliculas.csv";
 
+
+    public static boolean añadirPelicula(Pelicula pelicula){
+        return catalogo.add(pelicula);
+    }
+
+    public static void verCatalogo() {
+        if (catalogo.isEmpty()) {
+            System.out.println("El catálogo está vacío.");
+        } else {
+            System.out.println("Catálogo de películas:");
+            for (Pelicula p : catalogo) {
+                System.out.println("----------------------------------");
+                System.out.println("Título: " + p.getTitulo());
+                System.out.println("Director: " + p.getDirector());
+                System.out.println("Año de estreno: " + p.getEstreno());
+                System.out.println("Género: " + p.getGenero());
+                System.out.println("Valoración: " + p.getValoracion() + "/5");
+            }
+            System.out.println("----------------------------------");
+        }
+    }
+
+    public static void verMejorValoradas(){
+        catalogo.sort((a, b) -> Integer.compare(b.Valoracion, a.Valoracion));
+
+        catalogo.forEach(p -> System.out.println("Titulo: " + p.Titulo + " Valoración: " + p.Valoracion));
+    }
+
+    public static void filtrarPeliculasGenero(String g){
+        for (Pelicula pelito : catalogo) {
+            if (pelito.getGenero().toLowerCase().contains(g.toLowerCase())) {
+                System.out.println(pelito);
+            }
+        }
+    }
+
+    public static void filtrarPeliculasDirector(String d){
+        for (Pelicula pelito : catalogo) {
+            if (pelito.getDirector().toLowerCase().contains(d.toLowerCase())) {
+                System.out.println(pelito);
+            }
+        }
+    }
     public static void guardarCatalogo() {
         List<String> lineas = new ArrayList<>();
         for (Pelicula p : catalogo) {
@@ -19,14 +62,14 @@ public class GestorPeliculas {
             );
             lineas.add(linea);
         }
-//
+
         try {
             Path ruta = Paths.get(ARCHIVO);
             Files.createDirectories(ruta.getParent());
             Files.write(ruta, lineas);
-            System.out.println(" Catálogo guardado correctamente en " + ARCHIVO);
+            System.out.println("Catálogo guardado correctamente en " + ARCHIVO);
         } catch (IOException e) {
-            System.out.println("⚠ Error al guardar el catálogo: " + e.getMessage());
+            System.out.println("Error al guardar el catálogo: " + e.getMessage());
         }
     }
 
@@ -53,9 +96,12 @@ public class GestorPeliculas {
                     catalogo.add(new Pelicula(titulo, director, estreno, genero, valoracion));
                 }
             }
-            System.out.println(" Catálogo cargado correctamente (" + catalogo.size() + " películas)");
+            System.out.println(" Catalogo cargado correctamente (" + catalogo.size() + " películas)");
         } catch (IOException e) {
-            System.out.println("⚠ Error al cargar el catálogo: " + e.getMessage());
+            System.out.println(" Error al cargar el catálogo: " + e.getMessage());
         }
+
     }
+
+
 }
