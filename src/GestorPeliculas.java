c5502d453a235507f525840d2a9631116792d4eb
albcasmor3 <achantacalabuth@gmail.com>
2025-10-30 14:33:07 +0100

    Metodo para editar informacion

--- src/GestorPeliculas.java
+++ src/GestorPeliculas.java
@@ -1,28 +1,36 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public  class GestorPeliculas {
-    static List<pelicula> peliculas = new ArrayList<>();
+    static List<Pelicula> peliculas = new ArrayList<>();
 
-    public record pelicula(String titulo, String director, String genero, int duracion){
+    public static class Pelicula {
+        String titulo, director, genero;
+        int duracion;
 
+        public Pelicula(String titulo, String director, String genero, int duracion) {
+            this.titulo = titulo;
+            this.director = director;
+            this.genero = genero;
+            this.duracion = duracion;
+        }
     }
 
     static void modificarPeli(String d){
-        for (pelicula pelito : peliculas){
+        Scanner sc = new Scanner(System.in);
+        for (Pelicula superpeli : peliculas){
 
-            if (pelito.titulo.toLowerCase().contains(d.toLowerCase())){
+            if (superpeli.titulo.toLowerCase().contains(d.toLowerCase())){
                 int eleccion = 0;
                 System.out.println("¿Que quieres editar?");
                 System.out.println("1. Titulo 2. Director 3. Genero 4. Duracion");
                 switch (eleccion){
 
-                    case 1: pelito.titulo = sc.nextLine();
-                    case 2: pelito.director = sc.nextLine();
-                    case 3: pelito.genero = sc.nextLine();
-                    case 4: pelito.duracion = sc.nextInt();
-
-
+                    case 1: superpeli.titulo = sc.nextLine();
+                    case 2: superpeli.director = sc.nextLine();
+                    case 3: superpeli.genero = sc.nextLine();
+                    case 4: superpeli.duracion = sc.nextInt();
                     }
                 }
             }
